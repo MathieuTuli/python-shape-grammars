9ef80782a24f9cf0f4f5f16d113c105a36c0be38
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-07-30 12:37:40 -0400

    More heepful messages

--- src/python_shape_grammars/room.py
+++ src/python_shape_grammars/room.py
@@ -20,7 +20,7 @@ class Room(Rectangle):
         Rectangle.__init__(self, corners)
         if room_node.vector != self.midpoint:
             raise ValueError(
-                "For {str(self)} -" +
+                f"For {str(self)} -" +
                 " The room_node is not in the midpoint of the rectangle")
         self.name = name
         self.room_node = room_node
